1cefdb3d6885e7cdfd511ab1bb4f4c733649c2d4
Nikhil Sharma <n89sharma@gmail.com>
2026-02-14 00:42:36 -0500

    adjusted API for fast retrieval

--- src/services/api.ts
+++ src/services/api.ts
@@ -11,107 +11,64 @@ export const api = axios.create({
 })
 
 export interface AssetDetailResponse {
-    model: {
-        name: string,
-        brand: {
-            name: string
-        }
-    },
     barcode: string,
     serial_number: string,
+    model: string,
+    brand: string
     asset_type: string,
     tracking_status: string,
     availability_status: string,
     technical_status: string,
-    location: {
-        warehouse: {
-            city_code: string,
-            street: string
-        },
-        location: string
-    },
-    cost: {
-        purchase_cost: number,
-        transport_cost: number,
-        processing_cost: number,
-        other_cost: number,
-        parts_cost: number,
-        total_cost: number,
-        sale_price: number
-    },
-    technical_specification: {
-        cassettes: number,
-        internal_finisher: string,
-        meter_black: number,
-        meter_colour: number,
-        meter_total: number,
-        drum_life_c: number,
-        drum_life_m: number,
-        drum_life_y: number,
-        drum_life_k: number
-    },
-    hold: {
-        created_by: {
-            email: string,
-            name: string
-        },
-        created_for: {
-            email: string,
-            name: string
-        },
-        created_at: string,
-        customer: {
-            name: string
-        },
-        from_dt: string,
-        to_dt: string,
-        notes: string,
-        hold_number: string
-    },
+    location_city_code: string,
+    location_street: string,
+    location: string,
     created_at: string,
     is_held: boolean,
-    arrival: {
-        arrival_number: string,
-        origin: {
-            name: string
-        },
-        destination: {
-            city_code: string,
-            street: string
-        },
-        transporter: {
-            name: string
-        },
-        created_by: {
-            email: string,
-            name: string
-        },
-        notes: string,
-        created_at: string
-    },
-    departure: {
-        departure_number: string,
-        destination: {
-            name: string
-        },
-        origin: {
-            city_code: string,
-            street: string
-        },
-        transporter: {
-            name: string
-        },
-        created_by: {
-            email: string,
-            name: string
-        },
-        notes: string,
-        created_at: string
-    },
-    purchase_invoice: {
-        invoice_number: string,
-        is_cleared: boolean
-    }
+    purchase_cost: number,
+    transport_cost: number,
+    processing_cost: number,
+    other_cost: number,
+    parts_cost: number,
+    total_cost: number,
+    sale_price: number
+    ts_cassettes: number,
+    internal_finisher: string,
+    meter_black: string,
+    meter_colour: string,
+    drum_life_c: number,
+    drum_life_m: number,
+    drum_life_y: number,
+    drum_life_k: number,
+    hold_by_email: string,
+    hold_by_name: string,
+    hold_for_email: string,
+    hold_for_name: string,
+    hold_created_at: string,
+    hold_customer: string,
+    hold_from: string,
+    hold_to: string,
+    hold_notes: string,
+    hold_number: string,
+    arrival_number: string,
+    arrival_origin: string,
+    arrival_destination_city_code: string,
+    arrival_destination_street: string,
+    arrival_transporter: string,
+    arrival_created_by_email: string,
+    arrival_created_by_name: string,
+    arrival_notes: string,
+    arrival_created_at: string,
+    departure_number: string,
+    departure_origin_city_code: string,
+    departure_origin_street: string,
+    departure_destination: string,
+    departure_transporter: string,
+    departure_created_by_email: string,
+    departure_created_by_name: string,
+    departure_notes: string,
+    departure_created_at: string,
+    purchase_invoice_number: string,
+    purchase_invoice_is_cleared: string
 }
 
 export type AssetDetails = {
@@ -198,82 +155,83 @@ function formatUSD(value: number) {
     return currencyValue
 }
 
+function getFormattedDate(rawDate: string) {
+    return format(new Date(rawDate), 'MMMM dd, yyyy')
+}
 
 function mapAssetDetail(r: AssetDetailResponse): AssetDetails {
-    const rspec = r.technical_specification
-    const rcost = r.cost
     return {
-        model: r.model.name,
-        brand: r.model.brand.name,
         barcode: r.barcode,
         serial_number: r.serial_number,
+        model: r.model,
+        brand: r.brand,
         asset_type: r.asset_type,
         tracking_status: r.tracking_status,
         availability_status: r.availability_status,
         technical_status: r.technical_status,
-        location: r.location?.location,
-        warehouse_code: r.location?.warehouse.city_code,
-        warehouse_street: r.location?.warehouse.street,
+        location: r.location,
+        warehouse_code: r.location_city_code,
+        warehouse_street: r.location_street,
         cost: {
-            purchase_cost: formatUSD(rcost.purchase_cost),
-            transport_cost: formatUSD(rcost.transport_cost),
-            processing_cost: formatUSD(rcost.processing_cost),
-            other_cost: formatUSD(rcost.other_cost),
-            parts_cost: formatUSD(rcost.parts_cost),
-            total_cost: formatUSD(rcost.total_cost),
-            sale_price: formatUSD(rcost.sale_price)
+            purchase_cost: formatUSD(r.purchase_cost),
+            transport_cost: formatUSD(r.transport_cost),
+            processing_cost: formatUSD(r.processing_cost),
+            other_cost: formatUSD(r.other_cost),
+            parts_cost: formatUSD(r.parts_cost),
+            total_cost: formatUSD(r.total_cost),
+            sale_price: formatUSD(r.sale_price)
         },
         specs: {
-            cassettes: rspec.cassettes,
-            internal_finisher: rspec.internal_finisher,
-            meter_black: formatThousandsK(rspec.meter_black),
-            meter_colour: formatThousandsK(rspec.meter_colour),
-            meter_total: formatThousandsK(rspec.meter_total),
-            drum_life_c: rspec.drum_life_c,
-            drum_life_m: rspec.drum_life_m,
-            drum_life_y: rspec.drum_life_y,
-            drum_life_k: rspec.drum_life_k,
+            cassettes: r.ts_cassettes,
+            internal_finisher: r.internal_finisher,
+            meter_black: formatThousandsK(parseInt(r.meter_black)),
+            meter_colour: formatThousandsK(parseInt(r.meter_colour)),
+            meter_total: formatThousandsK(parseInt(r.meter_black) + parseInt(r.meter_colour)),
+            drum_life_c: r.drum_life_c,
+            drum_life_m: r.drum_life_m,
+            drum_life_y: r.drum_life_y,
+            drum_life_k: r.drum_life_k,
         },
-        hold: {
-            created_by: r.hold?.created_by?.name,
-            created_for: r.hold?.created_for?.name,
-            created_at: r.hold?.created_at ? format(new Date(r.hold.created_at), 'MMMM dd, yyyy') : null,
-            customer: r.hold?.customer?.name,
-            from_dt: r.hold?.from_dt,
-            to_dt: r.hold?.to_dt,
-            notes: r.hold?.notes,
-            hold_number: r.hold?.hold_number
-        },
-        created_at: format(new Date(r.created_at), 'MMMM dd, yyyy'),
+        created_at: getFormattedDate(r.created_at),
         is_held: r.is_held,
+        hold: {
+            created_by: r.hold_by_name,
+            created_for: r.hold_for_name,
+            created_at: !!r.hold_created_at ? getFormattedDate(r.hold_created_at) : null,
+            customer: r.hold_customer,
+            from_dt: r.hold_from,
+            to_dt: r.hold_to,
+            notes: r.hold_notes,
+            hold_number: r.hold_number
+        },
         arrival: {
-            arrival_number: r.arrival?.arrival_number,
-            origin: r.arrival?.origin.name,
-            destination_code: r.arrival?.destination.city_code,
-            destination_street: r.arrival?.destination.street,
-            transporter: r.arrival?.transporter.name,
-            created_by: r.arrival?.created_by.name,
-            notes: r.arrival?.notes,
-            created_at: r.arrival ? format(new Date(r.arrival.created_at), 'MMMM dd, yyyy') : ''
+            arrival_number: r.arrival_number,
+            origin: r.arrival_origin,
+            destination_code: r.arrival_destination_city_code,
+            destination_street: r.arrival_destination_street,
+            transporter: r.arrival_transporter,
+            created_by: r.arrival_created_by_name,
+            notes: r.arrival_notes,
+            created_at: !!r.arrival_created_at ? getFormattedDate(r.arrival_created_at) : ''
         },
         departure:{
-            departure_number: r.departure?.departure_number,
-            origin_code: r.departure?.origin.city_code,
-            origin_street: r.departure?.origin.street,
-            destination: r.departure?.destination.name,
-            transporter: r.departure?.transporter.name,
-            created_by: r.departure?.created_by.name,
-            notes: r.departure?.notes,
-            created_at: r.departure ? format(new Date(r.departure.created_at), 'MMMM dd, yyyy') : ''
+            departure_number: r.departure_number,
+            origin_code: r.departure_origin_city_code,
+            origin_street: r.departure_origin_city_code,
+            destination: r.departure_destination,
+            transporter: r.departure_transporter,
+            created_by: r.departure_created_by_name,
+            notes: r.departure_notes,
+            created_at: !!r.departure_created_at ? getFormattedDate(r.departure_created_at) : ''
         },
         purchase_invoice: {
-            invoice_number: r.purchase_invoice?.invoice_number,
-            is_cleared: String(r.purchase_invoice?.is_cleared)
+            invoice_number: r.purchase_invoice_number,
+            is_cleared: String(!!r.purchase_invoice_is_cleared)
         }
     }
 }
 
 export async function getAssetDetail(params: { barcode: string }): Promise<AssetDetails> {
-    const res = await api.get<AssetDetailResponse>(`/assets/${params.barcode}`)
-    return mapAssetDetail(res.data)
+    const res = await api.get<AssetDetailResponse[]>(`/fassets/${params.barcode}`)
+    return mapAssetDetail(res.data[0])
 }

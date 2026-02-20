5a6dc251f632ab6bc172dc77293b6fb812eda7fd
Nikhil Sharma <n89sharma@gmail.com>
2026-02-20 15:29:09 -0500

    quick search added

--- src/components/pages/arrivals.tsx
+++ src/components/pages/arrivals.tsx
@@ -7,7 +7,7 @@ import {
   PopoverContent,
   PopoverTrigger,
 } from "@/components/shadcn/popover"
-import { format } from "date-fns"
+import { format, subDays } from "date-fns"
 import { useArrivalStore } from "@/store/arrival-store"
 import { getArrivals } from "@/api/arrival"
 import type { Arrival } from "@/api/arrival"
@@ -82,9 +82,17 @@ export function ArrivalsPage() {
     setArrivals(await getArrivals(fromDate, toDate))
   }
 
+  async function handleQuickSearch(days: number) {
+    const from = subDays(new Date(), days)
+    const to = new Date()
+    setFromDate(from)
+    setToDate(to)
+    setArrivals(await getArrivals(from, to))
+  }
+
   return (
-    <div className="flex flex-col gap-4">
-      <FieldGroup className="flex flex-row gap-2 max-w-96">
+    <div className="flex flex-col gap-2">
+      <FieldGroup className="flex flex-row gap-2 max-w-96 border rounded-md p-2 items-end">
         <DatePickerField
           label="From Date"
           id="from-date"
@@ -100,9 +108,15 @@ export function ArrivalsPage() {
         />
 
         <Button
+          className="rounded-md"
           onClick={handleArrivalSearch}
         >Search</Button>
       </FieldGroup>
+      <div>
+        <Button variant="secondary" className="rounded-r-none" onClick={() => handleQuickSearch(1)}>1d</Button>
+        <Button variant="secondary" className="rounded-none" onClick={() => handleQuickSearch(7)}>7d</Button>
+        <Button variant="secondary" className="rounded-l-none" onClick={() => handleQuickSearch(30)}>30d</Button>
+      </div>
       <DataTable columns={columns} data={arrivals} />
     </div>
   )
@@ -117,19 +131,19 @@ interface DatePickerFieldProps {
 
 const DatePickerField = ({ label, date, setDate, id }: DatePickerFieldProps) => {
   return (
-    <Field className="mx-auto w-44">
-      <FieldLabel>{label}</FieldLabel>
+    <Field className="mx-auto w-44 gap-1">
+      <FieldLabel className="text-xs px-1">{label}</FieldLabel>
       <Popover>
         <PopoverTrigger asChild>
           <Button
             variant="outline"
             id={id}
-            className="justify-start text-muted-foreground text-xs"
+            className="justify-start text-muted-foreground text-xs rounded-md"
           >
             {date ? format(date, "PPP") : <span>Select</span>}
           </Button>
         </PopoverTrigger>
-        <PopoverContent className="w-auto p-0" align="start">
+        <PopoverContent className="w-auto p-1" align="start">
           <Calendar
             mode="single"
             selected={date}
